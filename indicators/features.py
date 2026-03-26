b389f3873586fb5e5a2a0023b46c97cfb34984cf
arulprakashka <720823106010@hit.edu.in>
2026-03-27 00:47:01 +0530

    Create features.py

---
+++ indicators/features.py
@@ -0,0 +1,13 @@
+# indicators/features.py
+# This file will contain all the detection functions (FVG, OB, etc.)
+# To keep it manageable, you can copy the entire set of detection functions from app.py
+# into this file. For brevity, I'm providing a placeholder.
+# In practice, you'd move all the detection functions from app.py here.
+
+def extract_features(high, low, close, volume, times):
+    """Run all pattern detectors and return features dict."""
+    # Placeholder – you need to copy your detection functions here
+    # and call them.
+    features = {}
+    # ... call your detectors ...
+    return features
